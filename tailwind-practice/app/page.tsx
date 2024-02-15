9ce4e38490b111ba7c982bfb6be56b80457d7f34
Pablo Gomez <pablogomez321@gmail.com>
2024-02-15 00:01:49 -0600

    tailwind practice

---
+++ tailwind-practice/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline text-lime-800 bg-violet-200 ">
+          Hello world!
+        </h1>
+        <p className="text-custom">Lorem, ipsum.</p>
+    </div>
+
+  );
+}
