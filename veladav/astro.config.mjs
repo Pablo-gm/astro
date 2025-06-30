805888f9821ddd6eab51da61c6647d8256209617
Pablo Gomez <pablogomez321@gmail.com>
2025-06-30 08:56:44 -0600

    boxers section completed

--- veladav/astro.config.mjs
+++ veladav/astro.config.mjs
@@ -2,10 +2,20 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  build: {
+    inlineStylesheets: 'always',
+  },
+  adapter: vercel(),
+  integrations: [sitemap()],
+  site: 'https://www.infolavelada.com/',
+
 });
